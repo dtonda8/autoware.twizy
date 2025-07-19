49a38010b7d53767cecb216d139e39568e36108d
dtonda8 <davidbatonda7@gmail.com>
2025-07-19 11:01:44 +1000

    Add extrinsic calibration script

---
+++ extrinsic_calib.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+source install/setup.bash && ros2 launch extrinsic_calibration_manager calibration.launch.xml mode:=manual sensor_model:=twizy_sensor_kit vehicle_model:=twizy_vehicle vehicle_id:=twizy
