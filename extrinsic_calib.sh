44c3bf388d7f7c022411d80012c5e8b08e710b53
dtonda8 <davidbatonda7@gmail.com>
2025-07-20 23:48:09 +0000

    Update calib script to output to main display

--- extrinsic_calib.sh
+++ extrinsic_calib.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export DISPLAY=:0
+
 source install/setup.bash && ros2 launch extrinsic_calibration_manager calibration.launch.xml mode:=manual sensor_model:=twizy_sensor_kit vehicle_model:=twizy_vehicle vehicle_id:=twizy
